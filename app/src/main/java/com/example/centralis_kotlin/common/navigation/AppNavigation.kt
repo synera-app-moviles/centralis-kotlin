9fb7e7608d4d3d6d7df7ba5fa67c1dc517eaf9b4
Neilcuri7 <u20231b866@upc.edu.pe>
2025-09-29 12:18:43 -0500

    refactor: Remove unused IamNavigation import from AppNavigation

--- app/src/main/java/com/example/centralis_kotlin/common/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/centralis_kotlin/common/navigation/AppNavigation.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.centralis_kotlin.iam.navigation.IamNavigation
 import com.example.centralis_kotlin.iam.presentation.views.SignInView
 import com.example.centralis_kotlin.iam.presentation.views.SignUpProfile
 import com.example.centralis_kotlin.iam.presentation.views.SignUpView
